2db954585825eb7e39bfe1bcdc577add79ced4db
tang-bf <1535710733@qq.com>
2020-08-20 23:57:30 +0800

    springmvc过程分析

--- src/main/java/springmvctestdemo/MyDispatcherServlet.java
+++ src/main/java/springmvctestdemo/MyDispatcherServlet.java
@@ -1,6 +1,7 @@
 package springmvctestdemo;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
