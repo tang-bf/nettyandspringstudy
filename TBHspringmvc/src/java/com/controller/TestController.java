5bb54aec3c610828a39eefdc164ccc2d99d8aaca
tang-bf <1535710733@qq.com>
2020-08-22 23:15:57 +0800

    jdk8后- -parameters参数 获取方法上参数名称

--- TBHspringmvc/src/java/com/controller/TestController.java
+++ TBHspringmvc/src/java/com/controller/TestController.java
@@ -17,6 +17,8 @@ public class TestController {
     @ResponseBody
     public Object test(String name, HttpServletRequest request, HttpServletResponse response, UserEntity userEntity){
         request.getParameter("name");
+        //Setting > Build,Execution,Depoyment > Compiler > Java Compiler ，在"Additional command line parameters"中加上"-parameters"参数
+        //需要jdk8指定-parameters 反射调用才能获取到方法上的参数名字，jdk7以前都是arg0 arg1
         System.out.println(name);
         System.out.println(request);
         System.out.println(response);
