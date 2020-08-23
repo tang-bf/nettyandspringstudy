123ecf65a76919da73fe885226570fef012f37d9
tang-bf <1535710733@qq.com>
2020-08-23 16:40:36 +0800

    tomcat添加context或者webapp形式的区别

--- src/main/java/springmvctestdemo/test/MyTomcatServer.java
+++ src/main/java/springmvctestdemo/test/MyTomcatServer.java
@@ -1,7 +1,9 @@
 package springmvctestdemo.test;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.Lifecycle;
 import org.apache.catalina.LifecycleException;
+import org.apache.catalina.LifecycleListener;
 import org.apache.catalina.startup.Tomcat;
 import org.springframework.context.event.ApplicationEventMulticaster;
 //import org.springframework.context.event.EventListener;
@@ -29,16 +31,20 @@ public class MyTomcatServer {
                 //EventListener
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(9898);
-        tomcat.addWebapp("/","d:\\tomc\\");
-       // tomcat.addContext()
-        try{
+        tomcat.addWebapp("/","d:\\tomc\\");//
+      // tomcat.addContext("/","") ;不会调用mydispatcherservlet的onstartup方法
+        // 只会去初始化一个context的资源目录  并不会加载web的声明周期
+        /**解决办法
+         * Context context = tomcat.addContext("/", "");
+         * context.addLifecycleListener((LifecycleListener) Class.forName(tomcat.getHost().getConfigClass()).newInstance());
+          */
+        try {
             tomcat.start();
-            //tomcat就是一个socket连接.所以要使其阻塞
-            tomcat.getServer().await();
-        }catch (LifecycleException e){
-            e.printStackTrace();
-        }finally{
-
+        } catch (LifecycleException ex) {
+            ex.printStackTrace();
         }
+        //tomcat就是一个socket连接.所以要使其阻塞
+        tomcat.getServer().await();
+
     }
 }
