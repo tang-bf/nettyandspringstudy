8be721743679001b585cc323db2559a52acf907c
tang-bf <1535710733@qq.com>
2020-08-24 21:29:10 +0800

    springmvc实现controller三种方式及对应解析mapping

--- src/main/java/springmvctestdemo/test/HandleController.java
+++ src/main/java/springmvctestdemo/test/HandleController.java
@@ -18,6 +18,10 @@ public class HandleController implements HttpRequestHandler {
     //默认会识别两种Controller
     //一种是以BeanName 声明的URL的的bean
     //一种是@Controller
+    //在dispatcherservlet中 getHandler handlerMappings 有
+    // requestmappinghandlemapping (mappingregistry registry(map requsetmappinginfo query.do bean=mycontroller method query  ))
+    // beannameurlhandlermapping (handlermap beannamecontroller user.do test3)
+    // getHandlerAdapter 会有三个requestmappinghandleradapter  httprequesthand...  simplecontrollerhand...
     @Override
     public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("111");
